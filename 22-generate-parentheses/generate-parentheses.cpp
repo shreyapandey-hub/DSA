deb10a4fbca7439ce71edb8d0df5796a5d9d824e
shreyapandey-hub <riinkupandey484@gmail.com>
2026-08-31 20:49:42 +0530

    Time: 3 ms (68.11%) | Memory: 12.9 MB (89.47%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void solve(int n, int open, int close,
+               string &s, vector<string>& ans) {
+
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        if (open < n) {
+            s.push_back('(');
+            solve(n, open + 1, close, s, ans);
+            s.pop_back();
+        }
+
+        if (close < open) {
+            s.push_back(')');
+            solve(n, open, close + 1, s, ans);
+            s.pop_back();
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s = "";
+
+        solve(n, 0, 0, s, ans);
+
+        return ans;
+    }
+};
